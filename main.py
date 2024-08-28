7f87b7afb85b141c985ab0c57891e6bf03a15021
dmitryk1210 <dmitryk1210@ya.ru>
2024-08-28 04:01:05 +0300

    random final changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 import requests
 import shutil
@@ -13,7 +14,7 @@ config        = None
 configPrivate = None
 
 
-CONFIG_FILE_NAME = "config.yaml"
+CONFIG_FILE_NAME         = "config.yaml"
 CONFIG_PRIVATE_FILE_NAME = "config.private.yaml"
 
 
@@ -27,12 +28,14 @@ def load_config():
             configPrivate = yaml.load(f, Loader=yaml.FullLoader)
 
 
-def get_cvar(name):
+def get_cvar(name, default = None):
     if config is None:
         load_config()
     if configPrivate is not None and configPrivate.get(name) is not None:
         return configPrivate.get(name)
-    return config.get(name)
+    if config.get(name) is not None:
+        return config.get(name)
+    return default
     
 
 def is_image_extension(extension: str) -> bool:
@@ -57,6 +60,12 @@ def query_yes_no(question):
             return False
 
 
+def filename_rm_invalid_symbols(filename):
+    INVALID_CHARS = r'\/:*?"<>|'
+    new_filename = ''.join('_' if char in INVALID_CHARS else char for char in filename)
+    return new_filename
+
+
 def auth_handler():
     """ 
     This function is called if two-factor authentication is required
@@ -68,66 +77,48 @@ def auth_handler():
     return key, remember_device
 
 
+def captcha_handler(captcha):
+    """ 
+    This function is called if captcha is required
+    """
+
+    key = input(f"Enter captcha code {captcha.get_url()}: ").strip()
+
+    return captcha.try_again(key)
+
+
+def find_best_resolution(images):
+    best_img = None
+    best_img_size = -1
+    for img in images:
+        current_img_size = img['height'] * img['width']
+        if current_img_size > best_img_size:
+            best_img_size = current_img_size
+            best_img = img
+    return best_img
+
+
 def process_photo(photo, chat_folder_full_path, urls_downloaded):
     strname = datetime.fromtimestamp(int(photo['date'])).strftime('IMG_%Y_%m_%d__%H_%M_%S')
-    # print(strname)
-    idx_best = 0
-    size_best = -1
-    for idx, photo_entity in enumerate(photo['sizes']):
-        size_current_entity = photo_entity['height'] * photo_entity['width']
-        if size_current_entity > size_best:
-            size_best = size_current_entity
-            idx_best = idx
-    best_entity = photo['sizes'][idx_best]
+    best_img = find_best_resolution(photo['sizes'])
     
     filepath = os.path.join(chat_folder_full_path, strname)
     if os.path.isfile(filepath + '.jpg'):
-        if best_entity['url'] in urls_downloaded:
+        if best_img['url'] in urls_downloaded:
             return
         i = 1
         while(os.path.isfile(f"{filepath}_{str(i)}.jpg")):
             i += 1
         filepath = f"{filepath}_{str(i)}"
 
-    raw_data = requests.get(best_entity['url']).content
+    raw_data = requests.get(best_img['url']).content
     with open(filepath + '.jpg', 'wb') as handler:
         handler.write(raw_data)
-    urls_downloaded.append(best_entity['url'])
+    urls_downloaded.append(best_img['url'])
 
-
-"""
-There you can add values that should be ignored:
-1 - text documents
-2 - archives
-3 - gif
-4 - image
-5 - audio
-6 - video
-7 - e-books
-8 - unknown
-"""
-DOC_IGNORE_BY_TYPE = [ 1, 2, 7 ]
-
-
-"""
-There you can add extensions (without commas) that should be saved.
-Files with other extensions will be ignored.
-For example:
-'jpg',
-'png',
-'tga'
-
-If there are no items in list, then all files will be saved
-"""
-DOC_FILTER_BY_TYPE = [ ]
-
-
-PEER_ID_LIST = [ ]
 
 def process_doc(doc, chat_folder_full_path, urls_downloaded):
-    if doc['type'] in DOC_IGNORE_BY_TYPE:
-        return
-    if len(DOC_FILTER_BY_TYPE) > 0 and (doc['ext'] not in DOC_FILTER_BY_TYPE):
+    if doc['type'] in get_cvar('doc_ignore_by_type', []):
         return
     
     prefix = 'DOC'
@@ -138,8 +129,8 @@ def process_doc(doc, chat_folder_full_path, urls_downloaded):
     else:
         return
     
-    strname = datetime.fromtimestamp(int(doc['date'])).strftime(f"{prefix}_%Y_%m_%d__%H_%M_%S_{doc['title']}")
-    # print(strname)
+    strtime = datetime.fromtimestamp(int(doc['date'])).strftime("%Y_%m_%d__%H_%M_%S")
+    strname = f"{prefix}_{strtime}_{filename_rm_invalid_symbols(doc['title'])}"
     
     filepath = os.path.join(chat_folder_full_path, strname)
     if os.path.isfile(filepath):
@@ -156,9 +147,42 @@ def process_doc(doc, chat_folder_full_path, urls_downloaded):
     urls_downloaded.append(doc['url'])
 
 
+def process_video(video, chat_folder_full_path, urls_downloaded, msg):
+    """
+    Fast hack - downloading only the preview and dumping message and video data. 
+    Need to find necessary videos manually. 
+    Removing duplicates is not supported yet.
+    """
+    video_folder = os.path.join(chat_folder_full_path, "video_info")
+    os.makedirs(video_folder, exist_ok=True)
+
+    strtime = datetime.fromtimestamp(int(msg['date'])).strftime("%Y_%m_%d__%H_%M_%S")
+    strname = f"VID_{strtime}_{filename_rm_invalid_symbols(video.get('title', ''))}"
+
+    filepath = os.path.join(video_folder, strname)
+    if os.path.isfile(filepath + '.json'):
+        i = 0
+        while(os.path.isfile(f"{filepath}_{str(i)}.json")):
+            i += 1
+        filepath = f"{filepath}_{str(i)}"
+    
+    with open(f"{filepath}.json", "w") as f:
+        f.write(f"vid: {json.dumps(video)}")
+
+    with open(f"{filepath}_msg.json", "w") as f:
+        f.write(f"vid: {json.dumps(msg)}")
+
+    first_frames_arr = video.get('first_frame')
+    if first_frames_arr is not None:
+        best_first_frame = find_best_resolution(first_frames_arr)
+        raw_data = requests.get(best_first_frame['url']).content
+        with open(f"{filepath}.jpg", 'wb') as handler:
+            handler.write(raw_data)
+
+
 def process_message(msg, chat_folder_full_path, urls_downloaded):
     """
-    documentation:
+    Documentation:
     https://dev.vk.com/ru/reference/objects/attachments-message
     """
     for attach in msg['attachments']:
@@ -166,12 +190,10 @@ def process_message(msg, chat_folder_full_path, urls_downloaded):
             process_photo(attach['photo'], chat_folder_full_path, urls_downloaded)
         if attach['type'] == 'doc':
             process_doc  (attach['doc'],   chat_folder_full_path, urls_downloaded)
-        if attach['type'] == 'video':
-            print(f"VIDEO IS NOT SUPPORTED, MSG INFO: id: {msg['id']}, date: {msg['date']}, peer_id: {msg['peer_id']}, from_id: {msg['from_id']}, text: {msg['text']}")
+        if attach['type'] == 'video' and get_cvar('process_video', False):
+            process_video(attach['video'], chat_folder_full_path, urls_downloaded, msg)
 
-    if msg.get('fwd_messages') is None:
-        return
-    for fwd_msg in msg['fwd_messages']:
+    for fwd_msg in msg.get('fwd_messages', []):
         process_message(fwd_msg, chat_folder_full_path, urls_downloaded)
 
 
@@ -179,7 +201,6 @@ def check_need_process_conversation(vk, conv_obj):
     peer_id     = conv_obj['conversation']['peer']['id']
     peer_type   = conv_obj['conversation']['peer']['type']
     chat_title  = ''
-    chat_folder = ''
     if peer_type == 'user':
         # should use 'profiles' from getConversations methon instead
         users_data = vk.users.get(
@@ -207,7 +228,6 @@ def check_need_process_conversation(vk, conv_obj):
 def process_conversation(vk, conv_obj):
     peer_id     = conv_obj['conversation']['peer']['id']
     peer_type   = conv_obj['conversation']['peer']['type']
-    chat_title  = ''
     chat_folder = ''
     if peer_type == 'user':
         # should use 'profiles' from getConversations methon instead
@@ -221,6 +241,7 @@ def process_conversation(vk, conv_obj):
         chat_folder = f"{peer_id}_group"
     else:
         chat_folder = f"{peer_id}_{peer_type}"
+    chat_folder = filename_rm_invalid_symbols(chat_folder)
 
     # get count of chat messages
     chat_attachments : dict = vk.messages.getHistory(
@@ -278,7 +299,8 @@ def main():
         login=login, 
         password=password,
         app_id=get_cvar('app_id'),
-        auth_handler=auth_handler
+        auth_handler=auth_handler,
+        captcha_handler=captcha_handler
     )
 
     try:
@@ -300,6 +322,8 @@ def main():
     total_count_conversations = conversations['count']
     CONVERSATIONS_COUNT_PER_REQUEST = 100 # max 200
 
+    peer_ids_to_process = get_cvar('peer_ids_to_process', [])
+
     offset = 0
     prepared_count_conversations = 0
     conversations_to_process = []
@@ -315,11 +339,12 @@ def main():
         
         for conv_obj in conversations.get('items'):
             prepared_count_conversations += 1
-            if len(PEER_ID_LIST) == 0:
+            if len(peer_ids_to_process) == 0:
                 if not check_need_process_conversation(vk, conv_obj):
                     continue
             else:
-                if conv_obj['conversation']['peer']['id'] not in PEER_ID_LIST:
+                peerId = conv_obj['conversation']['peer']['id']
+                if peerId not in peer_ids_to_process:
                     continue
             conversations_to_process.append(conv_obj)
     
